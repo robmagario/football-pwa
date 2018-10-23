e8080fb7c5cf9cf032c1954286f73880ba798cc4
robmagario <1ronison>
2018-10-23 19:24:20 +0800

    removed webpack and replaced it with Parcel

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const historyApiFallback = require('connect-history-api-fallback');
 const mongoose = require('mongoose');
 const path = require('path');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
+//const webpack = require('webpack');
+//const webpackDevMiddleware = require('webpack-dev-middleware');
+//const webpackHotMiddleware = require('webpack-hot-middleware');
 
 const config = require('../config/config');
-const webpackConfig = require('../webpack.config');
+//const webpackConfig = require('../webpack.config');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const port  = process.env.PORT || 8080;
@@ -29,12 +29,12 @@ app.use(express.json());
 require('./routes')(app);
 
 if (isDev) {
-  const compiler = webpack(webpackConfig);
+ // const compiler = webpack(webpackConfig);
 
   app.use(historyApiFallback({
     verbose: false
   }));
-
+/*
   app.use(webpackDevMiddleware(compiler, {
     publicPath: webpackConfig.output.publicPath,
     contentBase: path.resolve(__dirname, '../client/public'),
@@ -46,11 +46,25 @@ if (isDev) {
       chunkModules: false,
       modules: false
     }
-  }));
 
-  app.use(webpackHotMiddleware(compiler));
+  }));
+     */
+
+ // app.use(webpackHotMiddleware(compiler));
+  /*
+  app.use(require('webpack-hot-middleware')(compiler, {
+    log: console.log,
+    path: '/__webpack_hmr',
+    heartbeat: 10 * 1000
+  }));
   app.use(express.static(path.resolve(__dirname, '../dist')));
 } else {
+  app.use(require('webpack-hot-middleware')(compiler, {
+    log: console.log,
+    path: '/__webpack_hmr',
+    heartbeat: 10 * 1000
+  }));
+  */
   app.use(express.static(path.resolve(__dirname, '../dist')));
   app.get('*', function (req, res) {
     res.sendFile(path.resolve(__dirname, '../dist/index.html'));
